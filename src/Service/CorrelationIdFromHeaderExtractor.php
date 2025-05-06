83c950a9449eb81835fe7cf3cd35e028e4c9bcdf
Maksym Haiduk <maksym.haiduk@paysera.net>
2025-05-06 14:22:31 +0300

    CRV-1021: Implement fetching header via RequestStack

--- src/Service/CorrelationIdFromHeaderExtractor.php
+++ src/Service/CorrelationIdFromHeaderExtractor.php
@@ -5,13 +5,23 @@ declare(strict_types=1);
 namespace Paysera\LoggingExtraBundle\Service;
 
 use Paysera\LoggingExtraBundle\Listener\CorrelationIdListener;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class CorrelationIdFromHeaderExtractor
 {
+    private $requestStack;
+
+    public function __construct(RequestStack $requestStack) {
+        $this->requestStack = $requestStack;
+    }
+
     public function getCorrelationId(): ?string
     {
-        $headerName = 'HTTP_' . strtoupper(str_replace('-', '_', CorrelationIdListener::HEADER_NAME));
+        $request = $this->requestStack->getCurrentRequest();
+        if ($request === null) {
+            return null;
+        }
 
-        return $_SERVER[$headerName] ?? null;
+        return $request->headers->get(CorrelationIdListener::HEADER_NAME);
     }
 }
