5f0a3395daa27910ebbeb3ce406466830d148b3b
Maksym Haiduk <maksym.haiduk@paysera.net>
2025-05-28 13:40:19 +0300

    Get rid of conditional classes in CorrelationIdListener

--- src/Listener/CorrelationIdListener.php
+++ src/Listener/CorrelationIdListener.php
@@ -5,56 +5,29 @@ declare(strict_types=1);
 namespace Paysera\LoggingExtraBundle\Listener;
 
 use Paysera\LoggingExtraBundle\Service\CorrelationIdProvider;
-use Symfony\Component\HttpKernel\Event\FilterResponseEvent as LegacyResponseEvent;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
-if (class_exists(LegacyResponseEvent::class)) {
-    class CorrelationIdListener
+class CorrelationIdListener
+{
+    public const HEADER_NAME = 'Paysera-Correlation-Id';
+
+    private CorrelationIdProvider $correlationIdProvider;
+
+    public function __construct(CorrelationIdProvider $correlationIdProvider)
     {
-        const HEADER_NAME = 'Paysera-Correlation-Id';
-
-        private CorrelationIdProvider $correlationIdProvider;
-
-        public function __construct(CorrelationIdProvider $correlationIdProvider)
-        {
-            $this->correlationIdProvider = $correlationIdProvider;
-        }
-
-        public function onKernelResponse(LegacyResponseEvent $event)
-        {
-            if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
-                return;
-            }
-
-            $event->getResponse()->headers->set(
-                self::HEADER_NAME,
-                $this->correlationIdProvider->getCorrelationId()
-            );
-        }
+        $this->correlationIdProvider = $correlationIdProvider;
     }
-} else {
-    class CorrelationIdListener
+
+    public function onKernelResponse(ResponseEvent $event): void
     {
-        public const HEADER_NAME = 'Paysera-Correlation-Id';
-
-        private CorrelationIdProvider $correlationIdProvider;
-
-        public function __construct(CorrelationIdProvider $correlationIdProvider)
-        {
-            $this->correlationIdProvider = $correlationIdProvider;
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+            return;
         }
 
-        public function onKernelResponse(ResponseEvent $event): void
-        {
-            if (HttpKernelInterface::MAIN_REQUEST !== $event->getRequestType()) {
-                return;
-            }
-
-            $event->getResponse()->headers->set(
-                self::HEADER_NAME,
-                $this->correlationIdProvider->getCorrelationId()
-            );
-        }
+        $event->getResponse()->headers->set(
+            self::HEADER_NAME,
+            $this->correlationIdProvider->getCorrelationId()
+        );
     }
 }
